34c44a32a62746941e2923cb722b5506d2ae7b9a
Vishal Mittal <44922474+vismit2000@users.noreply.github.com>
2020-01-21 07:11:24 +0530

    Kadane Algorithm

---
+++ Maximum_Subarray_Sum.cpp
@@ -0,0 +1,16 @@
+// Kadane Algorithm - O(n)
+
+int MaxSubarraySum(vector <int> array)
+{
+    int n = array.size();
+
+    int best = 0, sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        sum = max(array[k], sum + array[k]);
+        best - max(best, sum);
+    }
+    
+    return best;
+}
