798c89670d02ddc564b8aafc245452db0ad20c8b
Vishal Mittal <44922474+vismit2000@users.noreply.github.com>
2020-02-04 14:02:31 +0530

    Update Knapsack.cpp

--- Knapsack.cpp
+++ Knapsack.cpp
@@ -2,20 +2,20 @@ int knapsackDP(int wt[],int price[],int N,int W)
 {
     int dp[100][100] = {0};
 
-    for(int n=0;n<=N;n++)
+    for(int n = 0; n <= N; n++)
     {
-        for(int w=0;w<=W;w++)
+        for(int w = 0; w <= W; w++)
         {
-            if(n==0||w==0)
+            if(n == 0 || w == 0)
                 dp[n][w] = 0;
             else
             {
-                int inc=0,exc = 0;
-                if(wt[n-1]<=w)             
-                    inc = price[n-1] + dp[n-1][w-wt[n-1]];                
+                int inc = 0, exc = 0;
+                if(wt[n-1] <= w)             
+                    inc = price[n-1] + dp[n-1][w - wt[n-1]];                
                 exc = dp[n-1][w];
 
-                dp[n][w] = max(inc,exc);
+                dp[n][w] = max(inc, exc);
             }
         }
     }
