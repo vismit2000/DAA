efa22b9fde42137c0893766f185dd63b87564d67
Vishal Mittal <44922474+vismit2000@users.noreply.github.com>
2020-02-27 08:50:41 +0530

    Edmond Karps algorithm for finding maximum flow in a network through Ford Fulkerson Algorithm
    https://cp-algorithms.com/graph/edmonds_karp.html

---
+++ Max_Flow_Ford_Fulk_Edmond_Karp.cpp
@@ -0,0 +1,57 @@
+// https://cp-algorithms.com/graph/edmonds_karp.html
+
+/*
+The matrix capacity stores the capacity for every pair of vertices. adj is the adjacency list of the undirected graph, 
+since we have also to use the reversed of directed edges when we are looking for augmenting paths.
+
+The function maxflow will return the value of the maximal flow. During the algorithm the matrix capacity will actually store
+the residual capacity of the network.
+*/
+
+int n;
+vector<vector<int>> capacity;
+vector<vector<int>> adj;
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && capacity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, capacity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> parent(n);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            capacity[prev][cur] -= new_flow;
+            capacity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
